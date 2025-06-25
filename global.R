7e8c0697723352a41efde31dfa5b863a579585cd
Patrick Thiel <patrickthiel@outlook.de>
2025-06-25 13:25:39 +0200

    Create global.R
    Script for global information like libraries and data that is needed in server.R.

---
+++ global.R
@@ -0,0 +1,34 @@
+#--------------------------------------------------
+# load libraries
+
+
+suppressPackageStartupMessages({
+    library(shiny)
+    library(htmltools)
+    library(shinyjs)
+    library(leaflet)
+    library(dplyr)
+    library(glue)
+    library(sf)
+    library(openxlsx)
+    library(data.table)
+    library(shinydashboard)
+    library(leafgl)
+    library(rmapshaper)
+    library(stringr)
+    library(glue)
+})
+
+#--------------------------------------------------
+# source config file
+
+source("config.R")
+
+#--------------------------------------------------
+# read data
+
+# PUF RWI-GEO-REDX data
+redx_data <- helpers_preparing_redx_data()
+
+# Essen centroid coordinates
+essen_centroid_coords <- helpers_essen_centroid()
