3d38198fd3de3ca9899d63825be670ae40f879c0
Patrick Thiel <patrickthiel@outlook.de>
2025-06-25 08:41:43 +0200

    Change script to function
    Which can be directly loaded in the server file. Saves the export/ reading of data.

--- helpers/helpers_essen_centroid.R
+++ helpers/helpers_essen_centroid.R
@@ -1,68 +1,61 @@
-#--------------------------------------------------
-# Description:
+helpers_essen_centroid <- function() {
+    #' @title Getting centroid of Essen
+    #' 
+    #' @description This function reads the shapefiles of the counties and finds
+    #' the centroid of the county of Essen.
+    #' 
+    #' @return Dataframe with centroid coordinates (longitude and latitude).
+    #' @author Patrick Thiel
 
-#' @title Getting centroid of Essen
-#' 
-#' @description This script read the shapefiles of the counties and finds the
-#' centroid of the county of Essen.
-#' 
-#' @author Patrick Thiel
+    #--------------------------------------------------
+    # libraries
 
-#--------------------------------------------------
-# libraries
+    suppressPackageStartupMessages({
+        library(sf)
+    })
 
-suppressPackageStartupMessages({
-    library(sf)
-})
+    #--------------------------------------------------
+    # source config file
 
-#--------------------------------------------------
-# source config file
+    source("config.R")
 
-source("config.R")
+    #--------------------------------------------------
+    # read counties
 
-#--------------------------------------------------
-# read counties
+    counties <- sf::st_read(
+        file.path(
+            config_paths()[["gebiete_path"]],
+            "Kreis",
+            "2019",
+            "VG250_KRS.shp"
+        ),
+        quiet = TRUE
+    ) |>
+    sf::st_transform(config_globals()[["gps_crs"]])
 
-counties <- sf::st_read(
-    file.path(
-        config_paths()[["gebiete_path"]],
-        "Kreis",
-        "2019",
-        "VG250_KRS.shp"
-    ),
-    quiet = TRUE
-) |>
-sf::st_transform(config_globals()[["gps_crs"]])
+    #--------------------------------------------------
+    # subset for Essen
 
-#--------------------------------------------------
-# subset for Essen
+    essen <- counties |>
+        dplyr::filter(
+            AGS == "05113"
+        )
 
-essen <- counties |>
-    dplyr::filter(
-        AGS == "05113"
-    )
+    #--------------------------------------------------
+    # calculate centroid
 
-#--------------------------------------------------
-# calculate centroid
+    essen_centroid <- sf::st_centroid(essen)
 
-essen_centroid <- sf::st_centroid(essen)
+    # extract coordinates
+    essen_centroid_coords <- sf::st_coordinates(essen_centroid) |>
+        as.data.frame() |>
+        dplyr::rename(
+            lon = X,
+            lat = Y
+        )
 
-# extract coordinates
-essen_centroid_coords <- sf::st_coordinates(essen_centroid) |>
-    as.data.frame() |>
-    dplyr::rename(
-        lon = X,
-        lat = Y
-    )
+    #--------------------------------------------------
+    # return
 
-#--------------------------------------------------
-# export
-
-data.table::fwrite(
-    essen_centroid_coords,
-    file.path(
-        config_paths()[["data_path"]],
-        "essen_centroid.csv"
-    ),
-    row.names = FALSE
-)
+    return(essen_centroid_coords)
+}
