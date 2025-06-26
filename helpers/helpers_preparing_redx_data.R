ecd325bf299d549fe17b6a5413dbabfe9447b71d
Patrick Thiel <patrickthiel@outlook.de>
2025-06-26 08:53:10 +0200

    Add helper for preparing RWI-GEO-REDX data
    Introduces a function to read, clean, and merge RWI-GEO-REDX data with grid and municipality information, returning a spatial dataframe for further plotting.

---
+++ helpers/helpers_preparing_redx_data.R
@@ -0,0 +1,86 @@
+helpers_preparing_redx_data <- function() {
+    #' @title Preparing RWI-GEO-REDX data
+    #' 
+    #' @description This function reads the RWI-GEO-REDX data and prepares it
+    #' for further plotting.
+    #' 
+    #' @return Spatial dataframe with RWI-GEO-REDX data and grid information.
+    #' @author Patrick Thiel
+    
+    #--------------------------------------------------
+    # source other files
+    
+    source("helpers/helpers_preparing_municipality_info.R")
+
+    #--------------------------------------------------
+    # read data
+
+    # PUF RWI-GEO-REDX data
+    redx_data <- openxlsx::read.xlsx(
+        file.path(
+            config_paths()[["redx_data"]],
+            paste0(
+                "RWIGEOREDX_GRIDS_v",
+                config_globals()[["redx_version"]],
+                "_PUF.xlsx"
+            )
+        ),
+        sheet = "Grids_RegionEff_yearly"
+    )
+
+    # grid information
+    grids_sf <- sf::st_read(
+        file.path(
+            config_paths()[["gebiete_path"]],
+            "Raster",
+            "ger_1km_rectangle",
+            "ger_1km_rectangle.shp"
+        ),
+        quiet = TRUE
+    )
+
+    # grid and municipality information
+    grids_munic <- helpers_preparing_municipality_info()
+
+    #--------------------------------------------------
+    # clean grids
+
+    grids_sf <- grids_sf |>
+        dplyr::select(-id) |>
+        sf::st_transform(config_globals()[["gps_crs"]])
+
+    #--------------------------------------------------
+    # clean REDX data
+
+    redx_data_prep <- redx_data |>
+        dplyr::select(-dplyr::contains("NOBS"))
+
+    # merge municipality information
+    redx_data_prep <- merge(
+        redx_data_prep,
+        grids_munic,
+        by = "grid",
+        all.x = TRUE
+    )
+
+    #--------------------------------------------------
+    # merge both datasets
+
+    redx_data_sf <- merge(
+        redx_data_prep,
+        grids_sf,
+        by.x = "grid",
+        by.y = "idm",
+        all.x = TRUE
+    )
+    
+    redx_data_sf <- sf::st_set_geometry(
+        redx_data_sf,
+        redx_data_sf$geometry
+    )
+
+    #--------------------------------------------------
+    # return
+
+    return(redx_data_sf)
+}
