8186149ee47a0840af64ab77d8d3297a1e1fbbac
Patrick Thiel <patrickthiel@outlook.de>
2025-09-03 15:24:19 +0200

    Switch to tab layout instead of dashboard

--- ui.R
+++ ui.R
@@ -1,42 +1,60 @@
 #--------------------------------------------------
 # start front end
 
-ui <- shinydashboard::dashboardPage(
-    shinydashboard::dashboardHeader(
-        title = "RWI-GEO-REDX"
-    ),
-
-    shinydashboard::dashboardSidebar(
-        collapsed = TRUE,
-        shinydashboard::sidebarMenu(
-            id = "sidebar",
-            shinydashboard::menuItem(
-                text = "Options",
-                icon = shiny::icon("gears")
+ui <- shiny::navbarPage(
+    title = "RWI-GEO-REDX",
+    id = "navbar",
+    shiny::tabPanel(
+        title = "Interactive Map",
+        div(
+            class = "outer",
+            tags$head(
+                shiny::includeCSS("www/styles.css")
+            ),
+            leaflet::leafletOutput("map", height = "100%", width = "100%"),
+            shiny::absolutePanel(
+                id = "options-map",
+                class = "panel panel-default", 
+                fixed = TRUE,
+                draggable = TRUE,
+                top = 60,
+                left = "auto",
+                right = 20,
+                bottom = "auto",
+                width = 340,
+                height = "auto",
+                h2("Map - Options"),
+                selectInput(
+                    inputId = "selecteded_housing_type",
+                    label = "Select housing type:",
+                    choices = list(
+                        "House sales" = "HK",
+                        "Apartment sales" = "WK",
+                        "Apartment rents" = "WM"
+                    ),
+                    # set default value
+                    selected = "WM"
+                ),
+                selectInput(
+                    inputId = "selected_year",
+                    label = "Select year:",
+                    choices = seq(2008, config_globals()[["max_year"]], by = 1),
+                    # set default value
+                    selected = config_globals()[["max_year"]]
+                )
             )
         ),
-        selectInput(
-            inputId = "selecteded_housing_type",
-            label = "Select housing type:",
-            choices = list(
-                "House sales" = "HK",
-                "Apartment sales" = "WK",
-                "Apartment rents" = "WM"
-            ),
-            # set default value
-            selected = "WM"
-        ),
-        selectInput(
-            inputId = "selected_year",
-            label = "Select year:",
-            choices = seq(2008, config_globals()[["max_year"]], by = 1),
-            # set default value
-            selected = config_globals()[["max_year"]]
+        tags$div(
+            id = "cite",
+            "Data source: RWI-GEO-REDX",
+            tags$a(
+                "RWI Real Estate Data",
+                href = "https://www.rwi-essen.de/en/research-advice/further/research-data-center-ruhr-fdz/data-sets/rwi-geo-red/x-real-estate-data-and-price-indices"
+            )
         )
     ),
-
-    shinydashboard::dashboardBody(
-        leaflet::leafletOutput("map", height = "100vh")
-        # tableOutput("tst")
+    shiny::tabPanel(
+        title = "Build Your Rent",
+        h2("Input your factors and calculate your rent/price")
     )
 )
