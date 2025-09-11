83304ad5f5cff1a3e18bbc44ef3ef04831d2901a
Patrick Thiel <patrickthiel@outlook.de>
2025-09-11 10:59:34 +0200

    Create helpers_preparing_model_coefs.R
    Function to prepare the model coefficients by combining them into one dataframe. The same is done for smearing factors.

---
+++ helpers/helpers_preparing_model_coefs.R
@@ -0,0 +1,105 @@
+helpers_preparing_model_coefs <- function() {
+    #' @title Prepare model coefficients and smearing factors
+    #' 
+    #' @description This function reads the model coefficients and smearing factors
+    #' for different housing types, combines them into single dataframes.
+    #' 
+    #' @return List containing dataframes for model coefficients and smearing factors.
+    #' @author Patrick Thiel
+
+    #--------------------------------------------------
+    # define housing types
+    
+    housing_types <-c("WM", "WK", "HK")
+
+    #--------------------------------------------------
+    # read all coefficients
+
+    coefs_list <- list()
+    for (housing_type in housing_types) {
+        # read data
+        coefs <- data.table::fread(
+            file.path(
+                config_paths()[["data_path"]],
+                paste0(
+                    "model_coefficients_year_absolute_",
+                    housing_type,
+                    ".csv"
+                )
+            )
+        )
+
+        # add housing type
+        coefs <- coefs |>
+            dplyr::mutate(
+                housing_type = housing_type
+            )
+
+        # store data
+        coefs_list[[housing_type]] <- coefs
+    }
+
+    # combine all data
+    coefs <- data.table::rbindlist(coefs_list)
+
+    # export
+    arrow::write_parquet(
+        coefs,
+        file.path(
+            config_paths()[["data_path"]],
+            "model_coefficients_prep.parquet"
+        )
+    )
+
+    #--------------------------------------------------
+    # read smearing factors
+
+    smearing_factors_list <- list()
+    for (housing_type in housing_types) {
+        # read data
+        smearing_factors <- data.table::fread(
+            file.path(
+                config_paths()[["data_path"]],
+                paste0(
+                    "smearing_factor_year_absolute_",
+                    housing_type,
+                    ".csv"
+                )
+            )
+        )
+
+        # add housing type
+        smearing_factors <- smearing_factors |>
+            dplyr::mutate(
+                housing_type = housing_type
+            )
+
+        # store data
+        smearing_factors_list[[housing_type]] <- smearing_factors
+    }
+
+    # combine all data
+    smearing_factors <- data.table::rbindlist(smearing_factors_list)
+
+    # export
+    arrow::write_parquet(
+        smearing_factors,
+        file.path(
+            config_paths()[["data_path"]],
+            "smearing_factors_prep.parquet"
+        )
+    )
+
+    #--------------------------------------------------
+    # store everything in a list
+
+    all_model_info <- list(
+        "coefs" = coefs,
+        "smearing_factors" = smearing_factors
+    )
+
+    #--------------------------------------------------
+    # return
+
+    return(all_model_info)
+}
