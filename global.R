4bb392b4ee3bf81300ec0e0f1b6e66f9447dbfc8
Patrick Thiel <patrickthiel@outlook.de>
2025-09-11 10:58:54 +0200

    Added reading model outputs to global

--- global.R
+++ global.R
@@ -17,6 +17,8 @@ suppressPackageStartupMessages({
     library(rmapshaper)
     library(stringr)
     library(glue)
+    library(shinyBS)
+    library(bslib)
 })
 
 #--------------------------------------------------
@@ -43,3 +45,10 @@ redx_data <- helpers_reading_prepared_redx()
 
 # Essen centroid coordinates
 essen_centroid_coords <- helpers_essen_centroid()
+
+# Model information
+hedonic_model_coefs <- helpers_reading_model_coefs()
+smearing_factors <- helpers_reading_smearing_factors()
+
+#--------------------------------------------------
+message("loaded global.R")
