a6d5fbc6047761d173db407f4219a82868fc23f9
Patrick Thiel <patrickthiel@outlook.de>
2025-09-11 11:00:50 +0200

    Create helpers_reading_smearing_factors.R
    Function to read the smearing factors into the application.

---
+++ helpers/helpers_reading_smearing_factors.R
@@ -0,0 +1,23 @@
+helpers_reading_smearing_factors <- function() {
+    #' @title Read smearing factors
+    #' 
+    #' @description This function reads the smearing factors from a parquet file.
+    #' 
+    #' @return Dataframe containing the smearing factors.
+    #' @author Patrick Thiel
+
+    #--------------------------------------------------
+    # read smearing factors
+
+    smearing_factors <- arrow::read_parquet(
+        file.path(
+            config_paths()[["data_path"]],
+            "smearing_factors_prep.parquet"
+        )
+    )
+
+    #--------------------------------------------------
+    # return
+
+    return(smearing_factors)
+}
