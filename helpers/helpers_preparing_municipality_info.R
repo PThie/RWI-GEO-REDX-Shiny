22bf70e44d7e36eca6a41ae141d966c056b1bbad
Patrick Thiel <patrickthiel@outlook.de>
2025-09-18 10:29:07 +0200

    Added city district and zipcode information

--- helpers/helpers_preparing_municipality_info.R
+++ helpers/helpers_preparing_municipality_info.R
@@ -31,6 +31,37 @@ helpers_preparing_municipality_info <- function() {
         quiet = TRUE
     )
 
+    # combination of grids and city districts
+    suppressWarnings(grids_city_districts <- data.table::fread(
+        file.path(
+            config_paths()[["gebiete_path"]],
+            "Zuordnung",
+            "Raster_Stadtteil",
+            "grid-centroid",
+            "Zuordnung_r1_stadtteil.csv"
+        )
+    ))
+
+    # combination of grids and zip-codes
+    grids_zip <- data.table::fread(
+        file.path(
+            config_paths()[["gebiete_path"]],
+            "Zuordnung",
+            "Raster_PLZ",
+            "raster_plz_2019_unambiguous.csv"
+        )
+    )
+
+    # city district names
+    city_district_names <- haven::read_dta(
+        file.path(
+            config_paths()[["gebiete_path"]],
+            "Stadtteile",
+            "_Stadtteile infas360",
+            "ortsteile_namen.dta"
+        )
+    )
+
     #--------------------------------------------------
     # clean combination data
 
@@ -41,6 +72,38 @@ helpers_preparing_municipality_info <- function() {
         ) |>
         dplyr::select(-share) |>
         dplyr::rename(grid = r1_id)
+
+    grids_city_districts <- grids_city_districts |>
+        dplyr::select(r1_id, ags11) |>
+        merge(
+            city_district_names |>
+                dplyr::select(city_district = ortsteil, ags11) |>
+                dplyr::mutate(
+                    city_district = dplyr::case_when(
+                        is.na(city_district) ~ "No information",
+                        TRUE ~ city_district
+                    )
+                ),
+            by = "ags11",
+            all.x = TRUE
+        ) |>
+        dplyr::select(-ags11)
+
+    # merge all
+    grids_munic <- grids_munic |>
+        merge(
+            grids_city_districts,
+            by.x = "grid",
+            by.y = "r1_id",
+            all.x = TRUE
+        ) |>
+        merge(
+            grids_zip |>
+                dplyr::rename(zipcode = PLZ),
+            by.x = "grid",
+            by.y = "idm",
+            all.x = TRUE
+        )
 
     #--------------------------------------------------
     # clean municipality information
